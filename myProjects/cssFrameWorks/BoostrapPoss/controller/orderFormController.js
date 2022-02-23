4513257a78e6237048fd0e8f20ba4076ec7eb266
TharinduDakshina <tharindudakshina527@gmail.com>
2022-02-24 01:01:21 +0530

    update

--- myProjects/cssFrameWorks/BoostrapPoss/controller/orderFormController.js
+++ myProjects/cssFrameWorks/BoostrapPoss/controller/orderFormController.js
@@ -1,9 +1,29 @@
 $("#placeOrderButton").click(function (){
     loadItemId();
+    loadCustomerId();
 });
 
 function  loadItemId(){
+    $("orderFormItemId").clear();
     itemDB.forEach(function (e){
         $("#orderFormItemId").append($("<option></option>").attr("value",e).text(e.getItemId()));
     });
 }
+
+function loadCustomerId(){
+    /*clearComboBox($("#orderFormCstId"));*/
+    customerDB.forEach(function (e){
+        $("#orderFormCstId").append($("<option></option>").attr("value",e).text(e.getCustomerId()));
+    });
+}
+
+/*function clearComboBox(selectElement) {
+    var i, L = selectElement.options.length - 1;
+    for (i = L; i >= 0; i--) {
+        selectElement.remove(i);
+    }
+}*/
+
+$("#orderFormCstId").selected(function (){
+    console.log("+++++++++++++++++++++++++++")
+});
