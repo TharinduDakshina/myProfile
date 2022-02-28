98d360654afde40d754c84daea7c28cf876e4b58
TharinduDakshina <tharindudakshina527@gmail.com>
2022-03-01 00:03:51 +0530

    declear oderDTO and update content

--- myProjects/cssFrameWorks/BoostrapPoss/controller/orderFormController.js
+++ myProjects/cssFrameWorks/BoostrapPoss/controller/orderFormController.js
@@ -72,18 +72,47 @@ function setItemData(id) {
 
 $("#btnAddItem").click(function () {
     countTotal();
+    saveOrder();
 });
+
+function saveOrder() {
+    var orderID=$("#orderId").val();
+    var date=$("#date").val();
+    var customerId=$("#orderFormCstId option:selected").text();
+    var itemId=$("#orderFormItemId option:selected").text();
+    var itemName=$("#orderFormItemName").val();
+    var itemPrice=$("#orderFormPrice").val();
+    var qty=$("#orderQty").val();
+    var total=$("#total").val();
+    var subTotal=$("#subTotal").val();
+
+
+}
+
 
 function countTotal() {
     var total;
-    if (parseInt($("#total").text()) == 0) {
+    var displayTotal=parseInt($("#total").text());
+    if ( displayTotal == 0) {
         total = (parseInt($("#orderQty").val())) * (parseInt($("#orderFormPrice").val()));
-        console.log(total);
+
         $("#total").text(total + ".00 /=");
     } else {
-        total = parseInt($("#total").text());
-        var sum = total + (parseInt($("#orderQty").val())) * (parseInt($("#orderFormPrice").val()));
+        var sum = displayTotal + (parseInt($("#orderQty").val())) * (parseInt($("#orderFormPrice").val()));
         $("#total").text(sum + ".00 /=");
     }
 
+    displayTotal=parseInt($("#total").text());
+
+    if (displayTotal > 100 || displayTotal < 1000){
+        $("#txtDiscount").val("5%");
+        var subTotal= displayTotal-((displayTotal * 5)/100);
+        $("#subTotal").text(subTotal);
+    }else if (displayTotal > 1000 ){
+        $("#txtDiscount").val("10%");
+        var subTotal= displayTotal-((displayTotal * 10)/100);
+        $("#subTotal").text(subTotal);
+    }
+
 }
+
