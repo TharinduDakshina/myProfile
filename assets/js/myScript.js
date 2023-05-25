efb9d09a1881be1a444566b772bb877b9a85513c
TharinduDakshina <tharindudaskhina527@gmail.com>
2023-05-25 21:36:08 +0530

    add toTop button

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -2,52 +2,61 @@ let regxName = /^[A-z ]{3,20}$/;
 let regxEmailAddress = /^[A-z 0-9@.,]{3,30}$/;
 
 
-
-$(window).on('load',function(){
+$(window).on('load', function () {
     $("#loader_content").fadeOut(1500);
 });
 
-$(document).ready(function (){
-
-    $(".darkModeButton").click(function (){
+$(document).ready(function () {
+    $(".darkModeButton").click(function () {
         $(".darkModeButtonInner").toggleClass("active");
         $("body,#homeMainSelection,#contactNavigationSelection,#about,#education,#skill,#projects,#gallery,#contact").toggleClass('dark');
     });
 });
 
-$("#formSubmit").attr('disabled',true);
+$("#formSubmit").attr('disabled', true);
 
 
 $('.carousel').flipster({
-    style:'carousel',
-    spacing:-0.01
+    style: 'carousel',
+    spacing: -0.01
 });
 
+const to_Top = $('.toTop');
 
-$("#email,#name,#feedback").on('keyup',function (){
+window.addEventListener('scroll',function (){
+
+    if (window.pageYOffset > 100) {
+        to_Top.addClass("active");
+    } else {
+        to_Top.removeClass("active");
+    }
+});
+
+$("#email,#name,#feedback").on('keyup', function () {
     contactValidation();
 });
+
 function contactValidation() {
-    var emailAddress=$("#email").val();
-    if (regxEmailAddress.test(emailAddress)){
-        $("#email").css('border','3px solid green');
-        var name=$("#name").val();
-        if (regxName.test(name)){
-            var feedBack =$('#feedback').val();
-            $("#name").css('border','3px solid green');
-            if (feedBack!=''){
-                $("#feedback").css('border','3px solid green');
-                $("#formSubmit").attr('disabled',false);
-            }else {
-                $("#feedback").css('border','3px solid red');
-                $("#formSubmit").attr('disabled',true);
+    var emailAddress = $("#email").val();
+    if (regxEmailAddress.test(emailAddress)) {
+        $("#email").css('border', '3px solid green');
+        var name = $("#name").val();
+        if (regxName.test(name)) {
+            var feedBack = $('#feedback').val();
+            $("#name").css('border', '3px solid green');
+            if (feedBack != '') {
+                $("#feedback").css('border', '3px solid green');
+                $("#formSubmit").attr('disabled', false);
+            } else {
+                $("#feedback").css('border', '3px solid red');
+                $("#formSubmit").attr('disabled', true);
             }
-        }else {
-            $("#name").css('border','3px solid red');
-            $("#formSubmit").attr('disabled',true);
+        } else {
+            $("#name").css('border', '3px solid red');
+            $("#formSubmit").attr('disabled', true);
         }
-    }else {
-        $("#email").css('border','3px solid red');
-        $("#formSubmit").attr('disabled',true);
+    } else {
+        $("#email").css('border', '3px solid red');
+        $("#formSubmit").attr('disabled', true);
     }
 }
